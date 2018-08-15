63d6043938a2de7afdfd18082fc41bb6125a97db
n00byD <nathan.devoll@gmail.com>
2018-08-15 16:34:14 -0500

    Update SV_CI_demo.bat

--- SV_CI_demo.bat
+++ SV_CI_demo.bat
@@ -1,4 +1,5 @@
 copy cars-web.properties C:\DevTest\DevTest\DemoServer\carsdemo\cars-web.properties
 start C:\DevTest\DevTest\DemoServer\carsdemo\start.bat
 PING 1.1.1.1 -n 1 -w 45000 >NUL
+start bzt CARS_UI_TEST.yaml
 bzt CARS_UI_TEST.yaml
